6999cb55f03c18fdccd39abc3b64ee9d096a432b
pollorijas <pollo.rijas@gmail.com>
2015-01-03 01:22:05 -0400

    Creacion de segunsa Activity Relacionada con el Login de la Alicación, Complementar con Cerrar la Aplicación al salir del Login.

--- SellPotato/app/src/main/java/sellpotato/sellpotato/LoginActivity.java
+++ SellPotato/app/src/main/java/sellpotato/sellpotato/LoginActivity.java
@@ -1,26 +1,24 @@
 package sellpotato.sellpotato;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
@@ -37,11 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void LaunchLoginActivity(View v)
-    {
-        Intent act = new Intent(this, LoginActivity.class);
-        startActivity(act);
     }
 }
