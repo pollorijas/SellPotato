d431df12adc1e63d6b7deb01b017a401e05ab2e8
vichopp <vichopp@gmail.com>
2015-03-09 16:31:11 -0300

    test

--- Prueba1/app/src/main/java/com/example/maria/prueba1/CancelarPedido.java
+++
@@ -1,9 +0,0 @@
-package com.example.maria.prueba1;
-
-import android.app.Activity;
-
-/**
- * Created by Maria on 09/02/2015.
- */
-public class CancelarPedido extends Activity {
-}
--- Prueba1/app/src/main/java/com/example/maria/prueba1/GenerarRecorrido.java
+++
@@ -1,7 +0,0 @@
-package com.example.maria.prueba1;
-
-/**
- * Created by Maria on 09/02/2015.
- */
-public class GenerarRecorrido {
-}
