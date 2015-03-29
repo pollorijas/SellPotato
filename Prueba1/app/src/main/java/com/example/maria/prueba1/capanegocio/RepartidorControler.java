046c20d2697ff535ed897d21de2f387990d91067
pollorijas <pollo.rijas@gmail.com>
2015-03-28 23:58:30 -0400

    repartidor controler

---
+++ Prueba1/app/src/main/java/com/example/maria/prueba1/capanegocio/RepartidorControler.java
@@ -0,0 +1,22 @@
+package com.example.maria.prueba1.capanegocio;
+
+/**
+ * Created by Freddy on 24-03-2015.
+ */
+public class RepartidorControler {
+
+    private Repartidor repartidor;
+
+    public Repartidor getRepartidor() {
+        return repartidor;
+    }
+
+    public void setRepartidor(Repartidor repartidor) {
+        this.repartidor = repartidor;
+    }
+
+    public void GenerarRuta()
+    {
+
+    }
+}
